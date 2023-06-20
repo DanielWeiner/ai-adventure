c4081c9381981dadbe211e3c8ee0d49e18672a86
Daniel Weiner <danielbweiner@gmail.com>
2023-06-20 14:40:24 -0500

    Fix improperly formatted Next links

--- app/create/creationPage.tsx
+++ app/create/creationPage.tsx
@@ -11,42 +11,42 @@ export default function CreationPage({ pageName, children } : { pageName: string
     return (
         <div className="flex flex-row w-full h-full">
             <nav className="flex flex-col flex-shrink bg-slate-400 text-slate-800">
-                <Link href="/create/world" data-active={pageName === 'world'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/world" data-active={pageName === 'world'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <WorldIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
                         Worlds
                     </span> 
                 </Link>
-                <Link href="/create/location" data-active={pageName === 'location'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/location" data-active={pageName === 'location'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <LocationIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
                         Locations
                     </span>
                 </Link>
-                <Link href="/create/faction" data-active={pageName === 'faction'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/faction" data-active={pageName === 'faction'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <FactionIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
                         Factions
                     </span>
                 </Link>
-                <Link href="/create/species" data-active={pageName === 'species'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/species" data-active={pageName === 'species'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <SpeciesIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
                         Species
                     </span>
                 </Link>
-                <Link href="/create/class" data-active={pageName === 'class'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/class"  data-active={pageName === 'class'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <ClassIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
                         Classes
                     </span>
                 </Link>
-                <Link href="/create/character" data-active={pageName === 'character'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
+                <Link href="/create/[pageName]" as="/create/character" data-active={pageName === 'character'} className="hs-tooltip [--placement:right] p-4 border-b-2 border-b-slate-500 [&[data-active=true]]:bg-slate-500">
                     <CharacterIcon className="hs-tooltip-toggle" size={iconSize} />
 
                     <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
