ee5876fbdc20bb8a8b9e870306e2028a34e333a7
Daniel Weiner <danielbweiner@gmail.com>
2023-06-24 15:31:22 -0500

    Responsive icons

--- app/create/creationPage.tsx
+++ app/create/creationPage.tsx
@@ -5,11 +5,9 @@ import { FactionIcon } from "../components/icons";
 import { LocationIcon, WorldIcon } from "../components/icons";
 import Link from "next/link";
 
-const iconSize = '48';
-
 const NavLink = ({ pageName, nounType, tooltip, Icon } : { pageName: string, nounType: string, tooltip: string, Icon: React.FC<{className?: string, size: string}>}) => (
     <Link href="/create/[pageName]" as={`/create/${nounType}`} {...pageName === nounType ? { 'aria-current': 'page' } : {} } className="hs-tooltip [--placement:bottom] lg:[--placement:right] [&[aria-current=page]]:bg-slate-500 p-2">
-        <Icon className="hs-tooltip-toggle" size={iconSize} />
+        <Icon className="hs-tooltip-toggle" size="auto" />
 
         <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-sm font-medium text-white rounded-md shadow-sm dark:bg-slate-700" role="tooltip">
             { tooltip }
@@ -21,7 +19,7 @@ export default function CreationPage({ pageName, children } : { pageName: string
     return (
         <div className="flex flex-col lg:flex-row w-full h-full">
             <div className="lg:max-w-[5rem] border-gray-200 lg:border-gray-300 dark:border-gray-700 border-b-2 lg:border-b-0">
-                <nav className="flex mr-0 lg:-mr-0.5 flex-row lg:flex-col space-y-0 lg:space-y-2 -mb-0.5 lg:mb-0 justify-evenly lg:justify-normal space-x-6 lg:space-x-0">
+                <nav className="flex mr-0 max-w-full lg:-mr-0.5 flex-row lg:flex-col space-y-0 lg:space-y-2 -mb-0.5 lg:mb-0 justify-evenly lg:justify-normal space-x-6 lg:space-x-0">
                     <NavLink Icon={WorldIcon} nounType="world" pageName={pageName} tooltip="Worlds" />
                     <NavLink Icon={LocationIcon} nounType="location" pageName={pageName} tooltip="Locations" />
                     <NavLink Icon={FactionIcon} nounType="faction" pageName={pageName} tooltip="Factions" />
