35c6b143d5a4be26ee3561fca7092b0a66b1488b
Daniel Weiner <danielbweiner@gmail.com>
2023-07-30 00:20:56 -0500

    ditch lerna

--- packages/ai-queue/docker/start_dev_server.sh
+++ packages/ai-queue/docker/start_dev_server.sh
@@ -3,14 +3,14 @@ set -Eeuo pipefail
 trap "kill 0" EXIT TERM INT ABRT
 
 npx yarn workspace ai-queue install
-npx lerna run --scope=ai-queue build
+npx yarn workspace ai-queue run build
 (
     set -euo pipefail
     npx nodemon --inspect=0.0.0.0:9229 packages/ai-queue/.ai-queue/server.js 
 ) &
 (
     set -euo pipefail
-    npx lerna run --scope=ai-queue watch
+    npx yarn workspace ai-queue run watch
 ) &
 
 wait -n
--- packages/app/docker/start_dev_server.sh
+++ packages/app/docker/start_dev_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-NODE_OPTIONS='--inspect=9230 --inspect-port=0.0.0.0:9230' node --inspect-port=0.0.0.0:9234 $(which npx) lerna run --scope=ai-adventure-app dev
+NODE_OPTIONS='--inspect=9230 --inspect-port=0.0.0.0:9230' node --inspect-port=0.0.0.0:9234 $(which npx) yarn workspace ai-adventure-app run dev
