3d178714ffb0b71e469b87bd4642c154aade2a02
Daniel Weiner <danielbweiner@gmail.com>
2023-08-01 14:43:11 -0500

    Pass execution of ai-queue off to npm

--- packages/ai-queue/docker/start_dev_server.sh
+++ packages/ai-queue/docker/start_dev_server.sh
@@ -2,7 +2,6 @@
 set -Eeuo pipefail
 trap "kill 0" EXIT TERM INT ABRT
 
-pnpm --filter ai-queue build
 (
     set -euo pipefail
     pnpm --filter ai-queue exec nodemon --inspect=0.0.0.0:9229 $(which npm) run start
